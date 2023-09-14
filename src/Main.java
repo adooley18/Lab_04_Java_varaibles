d65ff3e92fe0d3a3ce9bd921cb2deb4b7b6901c5
ajdooley18 <ajdooley16@gmail.com>
2023-09-14 15:22:56 -0400

    First Java. Part 1!

--- src/Main.java
+++ src/Main.java
@@ -43,6 +43,42 @@ public class Main {
         System.out.println("The product of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct);
 
         doubleDifference = doubleOperandA - doubleOperandB;
-        System.out.println("The product of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference);
+        System.out.println("The difference of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference);
+
+        doubleQuotient = doubleOperandA / doubleOperandB;
+        System.out.println("The quotient of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleQuotient);
+
+        doubleModulo = doubleOperandA % doubleOperandB;
+        System.out.println("The modulo of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleModulo);
+
+        //the number of kids in your family:  int numberKids = 2;
+        //the answer to is it raining? :  Boolean isRaining = false;
+        //the price of a gallon of gas :  double priceGas = 2.50;
+        //your favorite number :  int favoritNumber = 7;
+        //your shoe size:  double shoesize = 10.5;
+        //your birth month:  string birthMonth = “March”
+        //your full name:  string fullName = “Aidan Dooley”
+
+        int numberKids = 3;
+        System.out.println("The number of kids in my family is " + numberKids);
+
+        boolean isRaining = false;
+        System.out.println("It is " + isRaining + " to say it is raining.");
+
+        int favoritNumber = 5;
+        System.out.println("My favorit number is " + favoritNumber);
+
+        //double priceGas = 0;
+        //int favoritNumber = 0;
+        //double shoeSize = 0;
+        //String birthMonth = 0;
+        //String fullName = 0;
+
+
+
+
+
+
+
     }
 }
